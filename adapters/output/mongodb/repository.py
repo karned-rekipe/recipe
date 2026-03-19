2a55bff1ac2766d1deee606b59584b7c9abef742
sheligon <heligon.seb@gmail.com>
2026-03-19 21:26:36 +0100

    feat: Add MongoDB step repository to the repository module
    Signed-off-by: sheligon <heligon.seb@gmail.com>

--- adapters/output/mongodb/repository.py
+++ adapters/output/mongodb/repository.py
@@ -1,9 +1,11 @@
 from adapters.output.mongodb.ingredient_repository import MongoDBIngredientRepository
 from adapters.output.mongodb.recipe_repository import MongoDBRecipeRepository
+from adapters.output.mongodb.step_repository import MongoDBStepRepository
 from adapters.output.mongodb.ustensil_repository import MongoDBUstensilRepository
 
 __all__ = [
     "MongoDBIngredientRepository",
     "MongoDBRecipeRepository",
     "MongoDBUstensilRepository",
+    "MongoDBStepRepository",
 ]
