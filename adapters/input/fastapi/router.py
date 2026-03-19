bb5054ae8c7230b8bc5071b033696c95ee9882b9
sheligon <heligon.seb@gmail.com>
2026-03-19 21:24:11 +0100

    feat: Add step router and service integration to the main router

--- adapters/input/fastapi/router.py
+++ adapters/input/fastapi/router.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 from arclith import Arclith
 from adapters.input.fastapi.ingredient_router import IngredientRouter
 from adapters.input.fastapi.recipe_router import RecipeRouter
+from adapters.input.fastapi.step_router import StepRouter
 from adapters.input.fastapi.ustensil_router import UstensilRouter
 from infrastructure.ingredient_container import build_ingredient_service
 from infrastructure.recipe_container import build_recipe_service
+from infrastructure.step_container import build_step_service
 from infrastructure.ustensil_container import build_ustensil_service
 
 
@@ -13,6 +15,8 @@ def register_routers(app: FastAPI, arclith: Arclith) -> None:
     ingredient_service, logger = build_ingredient_service(arclith)
     recipe_service, _ = build_recipe_service(arclith)
     ustensil_service, _ = build_ustensil_service(arclith)
+    step_service, _ = build_step_service(arclith)
     app.include_router(IngredientRouter(ingredient_service, logger).router)
     app.include_router(RecipeRouter(recipe_service, logger).router)
     app.include_router(UstensilRouter(ustensil_service, logger).router)
+    app.include_router(StepRouter(step_service, logger).router)
